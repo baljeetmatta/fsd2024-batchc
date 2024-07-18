60049c70f75e6ac5616f7c0d4cdfd66c5febd67d
RebootTalents <168439890+RebootTalents@users.noreply.github.com>
2024-07-18 10:42:09 +0530

    dom-1

---
+++ js/dom.js
@@ -0,0 +1,29 @@
+//console.log(document.all[4]);
+//document.write("Hello");
+
+//var elements=document.getElementsByTagName("p");
+///console.log(elements[1].innerText);
+//elements[1].innerHTML="Demo <b>New Content</b>";
+//var element=document.getElementById("mypara");
+//element.innerHTML="Dynamic javascript content";
+// var elements=document.getElementsByClassName("design");
+// for(i=0;i<elements.length;i++)
+//     {
+//         //elements[i].style.add ("background-color","red");
+//         elements[i].setAttribute("style","background-color:red");
+
+
+//     }
+//var elements=document.getElementsByClassName
+// elements.forEach(element => {
+//     console.log(element);
+
+// // });
+// var elements=document.querySelectorAll(".design");
+// console.log(elements);
+// elements.forEach(function(item){
+//     console.log(item);
+
+// })
+var box=document.querySelector("#box");
+box.value="Initial";
