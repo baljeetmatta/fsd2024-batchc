78f648b85b79003c196ef860fd0ee4a79efc9ff6
RebootTalents <168439890+RebootTalents@users.noreply.github.com>
2024-07-24 10:47:50 +0530

    Dom events

--- js/dom.js
+++ js/dom.js
@@ -25,5 +25,61 @@
 //     console.log(item);
 
 // })
-var box=document.querySelector("#box");
-box.value="Initial";
+// var box=document.querySelector("#box");
+// box.value="Initial";
+
+// let mypara=document.querySelector("#mypara");
+// //mypara.setAttribute("data","test");
+// //mypara.setAttribute("class","x")
+// //console.log(mypara.classList)
+// //mypara.classList.add("z");
+// //mypara.classList.toogle("x");
+// function test()
+// {
+//     console.log("Para clicked");
+
+// }
+// function clickHandler()
+// {
+//     console.log("Button clicked");
+
+// }
+
+// let btn=document.querySelector("#btn");
+// let btn1=document.querySelector("#btn1");
+// btn.addEventListener("click",clickHandler);
+// btn1.addEventListener("click",clickHandler);
+
+// function clickHandler(e)
+// {
+//     console.log("Button clicked");
+//     console.log(e.target);
+// }
+
+// btn.addEventListener("click",function(){
+//     console.log("Button clicked");
+// })
+
+
+let digits=document.querySelector("#digits")
+// let btn1=document.querySelector("#btn1");
+// let btn2=document.querySelector("#btn2");
+// let btn3=document.querySelector("#btn3");
+// btn1.addEventListener("click",clickHandler);
+// btn2.addEventListener("click",clickHandler);
+// btn3.addEventListener("click",clickHandler);
+
+let btns=document.querySelectorAll(".btn");
+
+btns.forEach(function(item){
+    item.addEventListener("click",clickHandler);
+})
+
+function clickHandler(e)
+{
+    if(digits.innerText=="0")
+        digits.innerText="";
+    
+    digits.innerText=digits.innerText+ e.target.innerText;
+
+}
